f3b1c6638e907e8a5cdcd583495a65f7f86cdd58
Eavan Pattie <eavan@lyse.net>
2017-09-24 20:56:05 +0200

    added training loop

--- train.py
+++ train.py
@@ -7,7 +7,7 @@ USE_CUDA = torch.cuda.is_available()
 TRAIN_LOADER = torch.utils.data.DataLoader(
         datasets.CIFAR10('./data', train=True, download=True,
                         transform=transforms.ToTensor()))
-PRINT_EVERY = 500
+PRINT_EVERY = 50
 
 def setup(learning_rate=1e-3):
     model = ChainedAutoencoder()
@@ -25,12 +25,12 @@ def get_optimizer(model, learning_rate=1e-3):
     return torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 def train_example(model, image_tensor, optimizer, criterion, num_chains=1):
-    image_variable = Variable(image_tensor)
+    image_variable = Variable(image_tensor, requires_grad=False)
     if USE_CUDA:
         image_variable = image_variable.cuda()
     image_output = model(image_variable, num_chains=num_chains)
-    loss = criterion(image_variable, image_output)
-    loss.backwards()
+    loss = criterion(image_output, image_variable)
+    loss.backward()
     optimizer.step()
     optimizer.zero_grad()
     return loss
@@ -42,18 +42,19 @@ if __name__ == '__main__':
     chained_model, chained_optimizer, _ = setup()
     chained_losses = []
 
+    print("Starting Training")
     for iter_id, (image_tensor, label) in enumerate(TRAIN_LOADER):
         unchained_losses.append(train_example(
-            unchained_model, image_tensor, unchained_optimizer, criterion))
+            unchained_model, image_tensor, unchained_optimizer, criterion).data[0])
 
         num_chains = (iter_id // 10000) + 1
         chained_losses.append(train_example(
             chained_model, image_tensor, chained_optimizer,
-            criterion, num_chains=num_chains))
+            criterion, num_chains=num_chains).data[0])
 
 
         if (iter_id + 1) % PRINT_EVERY == 0:
-            print("Current unchained loss is {}".format(
+            print("Current average unchained loss is {}".format(
                 sum(unchained_losses[-PRINT_EVERY:]) / PRINT_EVERY))
             print("Current average {}-chained loss is {}".format(
                 num_chains, sum(chained_losses[-PRINT_EVERY:]) / PRINT_EVERY))
