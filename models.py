9b56356b5951010c2a5d4c395bed930c0ba17edb
Eavan Pattie <eavan@lyse.net>
2017-09-25 00:36:50 +0200

    refactored models

--- models.py
+++ models.py
@@ -1,7 +1,15 @@
+"""
+Module contains models to be used during training
+"""
+
 import torch
 from torch.autograd import Variable
 
 class ChainedAutoencoder(torch.nn.Module):
+    """
+    Class for chaining together multiple autoencoders
+    such that the output of the first becomes the input of the second
+    """
     def __init__(self):
         super(ChainedAutoencoder, self).__init__()
 
@@ -15,17 +23,24 @@ class ChainedAutoencoder(torch.nn.Module):
                             for i in range(len(dims) - 1)])
 
     def forward(self, image_tensor, num_chains=1):
+        # Store batch size for later reconstruction after flattening tensor
         batch_size = len(image_tensor)
         flat_tensor = image_tensor.view(batch_size, -1) # flattens tensor, with batch
 
-        for chain_id in range(num_chains):
+        # Loop over the number of times to chain
+        for _ in range(num_chains):
             for linear in self.linear_transforms:
                 flat_tensor = linear(flat_tensor)
                 flat_tensor = self.relu(flat_tensor)
 
-        return flat_tensor.view(batch_size,3,32,32)
+        return flat_tensor.view(batch_size, 3, 32, 32)
+
+def test():
+    """ Test for class creation and forward method """
+    model = ChainedAutoencoder()
+    input_tensor = torch.FloatTensor(1, 3, 32, 32)
+    model(Variable(input_tensor), 3)
+    print("Test Success")
 
 if __name__ == '__main__':
-    model = ChainedAutoencoder()
-    x = torch.FloatTensor(1,3,32,32)
-    y = model(Variable(x), 1)
+    test()
